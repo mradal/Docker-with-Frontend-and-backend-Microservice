2dc19a16bcd33f5cfb56f3d3041b0b0e7a261d0d
root <root@ip-172-31-44-227.ap-south-1.compute.internal>
2022-01-16 22:17:55 +0000

    By Mradal Tiwari

---
+++ .bash_history
@@ -0,0 +1,6 @@
+sudo su
+sudo su
+docker ps 
+service docker start
+service start docker
+sudo su
---
+++ insertvalue.sql
@@ -0,0 +1,7 @@
+INSERT INTO priarworld (firstname,rollnumber) VALUES ('mradal',1);
+INSERT INTO priarworld (firstname,rollnumber) VALUES ('raghav',2);
+INSERT INTO priarworld (firstname,rollnumber) VALUES ('upanshu',3);
+INSERT INTO priarworld (firstname,rollnumber) VALUES ('ayush',4);
+INSERT INTO priarworld (firstname,rollnumber) VALUES ('jatin',5);
+
+
